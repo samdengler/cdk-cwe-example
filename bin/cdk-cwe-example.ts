2c300122080b39a5aea2337868dc80c490985d1e
Sam Dengler <fsd@amazon.com>
2019-06-25 22:02:39 -0400

    add account / region

--- bin/cdk-cwe-example.ts
+++ bin/cdk-cwe-example.ts
@@ -6,9 +6,10 @@ import { CdkCweExampleStack } from '../lib/cdk-cwe-example-stack';
 const app = new cdk.App();
 
 new CdkCweExampleStack(app, 'dev', {
+  env: { account: '375027758210', region: 'us-east-1' },
   s3Bucket: 'fsd-demo-dev'
 });
 
-new CdkCweExampleStack(app, 'prod', {
-  s3Bucket: 'fsd-demo-prod'
-});
+// new CdkCweExampleStack(app, 'prod', {
+//   s3Bucket: 'fsd-demo-prod'
+// });
